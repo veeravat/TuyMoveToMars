736baf3fa2aa75123e21e8c1d17dd8dff8da6f0e
veeravat <veeravat@me.com>
2017-04-26 19:19:50 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Reference the packages we require so that we can use them in creating the bot
+var restify = require('restify');
+var builder = require('botbuilder');
+// =========================================================
+// Bot Setup
+// =========================================================
+
+// Setup Restify Server
+// Listen for any activity on port 3978 of our local server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
+});
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+
+// If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle
+server.post('/api/messages', connector.listen());
+// =========================================================
+// Bots Dialogs 
+// =========================================================
+// This is called the root dialog. It is the first point of entry for any message the bot receives
+bot.dialog('/', function(session) {
+    // Send 'hello world' to the user
+    session.send("Hello World");
+});
